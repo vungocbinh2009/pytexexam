215df4313c182b0397ef50001468ffec8aa35b62
vungocbinh2009 <55776093+vungocbinh2009@users.noreply.github.com>
2019-11-30 10:28:33 +0700

    Change __init__.py file. Change README.md file.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pytexexam',
-    version='1.2',
+    version='1.2.1',
     packages=['pytexexam'],
     url='https://github.com/vungocbinh2009/pytexexam',
     license='Apache License, Version 2.0',
