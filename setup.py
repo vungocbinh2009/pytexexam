09054a5590bfaacf45c0097ac3d31c4b880eec67
binh <vungocbinh@protonmail.com>
2021-07-06 20:58:40 +0700

    Version 2.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pytexexam',
-    version='2.1.0',
+    version='2.1.1',
     packages=['pytexexam'],
     include_package_data=True,
     url='https://github.com/vungocbinh2009/pytexexam',
